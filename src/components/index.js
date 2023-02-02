b956b84806edb2a061ae0f86a7aa2cc82165cc7c
evan <evan.kusyanto@students.amikom.ac.id>
2023-02-03 01:45:03 +0700

    react-hoobank

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA.jsx";
+import FeedBackCard from "./FeedBackCard";
+import Getstarted from "./Getstarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonial from "./Testimonial";
+import Footer from "./Footer";
+import Bussines from "./Bussines";
+
+export {
+  Billing,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Getstarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonial,
+  Footer,
+  Bussines,
+};
